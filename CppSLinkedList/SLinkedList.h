950655bd9455ef27d99c728fb885d71508869264
osst1652 <oskar.steinhauf@gmail.com>
2017-04-01 18:45:33 +0200

    Header file commit

--- CppSLinkedList/SLinkedList.h
+++ CppSLinkedList/SLinkedList.h
@@ -1,9 +1,37 @@
 #ifndef SLINKEDLIST_H
 #define SLINKEDLIST_H
+
+#include <iostream>
+/*
+	Nod
+*/
+struct node 
+{
+	int data;
+	std::string text;
+	node * next; 
+};
+
+/*
+	Linked list header
+*/
 class SLinkedList
 {
+private:
+	node * head;
+	int listLength;
+
 public:
+	//Default constructor
 	SLinkedList();
+	
+	bool add(node* newNode, int pos);
+
+	bool remove(int pos);
+
+	void print();
+
+	//Default destructor
 	~SLinkedList();
 };
 
